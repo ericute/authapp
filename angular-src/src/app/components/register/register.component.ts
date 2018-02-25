7d84a6af66736daf94c2f73bf939005ff22867fb
ericute <ericjohnleron@gmail.com>
2018-02-26 06:18:33 +0800

    Added angular source codes.

---
+++ angular-src/src/app/components/register/register.component.ts
@@ -0,0 +1,67 @@
+import { Component, OnInit } from '@angular/core';
+
+import { ValidateService } from '../../services/validate.service';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { AuthService } from '../../services/auth.service';
+
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+
+  name: String;
+  username: String;
+  email: String;
+  password: String;
+
+  constructor(
+    private validateService : ValidateService,
+    private flashMessagesService : FlashMessagesService,
+    private authService : AuthService,
+    private router : Router
+  ) { }
+
+  ngOnInit() {
+  }
+
+  onRegisterSubmit() {
+    //console.log(this.name);
+    const user = {
+      name: this.name,
+      email: this.email,
+      username: this.username,
+      password: this.password
+    }
+
+    // Required Fields
+    if (!this.validateService.validateRegister(user)) {
+      //console.log('Please fillout all fields.');
+      this.flashMessagesService.show('Please fillout all fields.', {cssClass: 'alert-danger', timeout: 10000});
+      return false;
+    }
+
+    // Validate Email
+    if (!this.validateService.validateEmail(user.email)) {
+      //console.log('Please use a valid email!');
+      this.flashMessagesService.show('Please use a valid email!', {cssClass: 'alert-danger', timeout: 10000});
+      return false;
+    }
+
+    // Register User
+    this.authService.registerUser(user).subscribe(data => {
+      if (data.success) {
+        this.flashMessagesService.show('User successfully registered!', {cssClass: 'alert-success', timeout: 10000});
+        this.router.navigate(['/login']);
+      } else {
+        this.flashMessagesService.show('Failed to register user!', {cssClass: 'alert-danger', timeout: 10000});
+        this.router.navigate(['/register']);
+      }
+    });
+
+  }
+
+}
--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 module.exports = {
-    database: 'mongodb://localhost:27017/impactanalyzer',
+    //database: 'mongodb://localhost:27017/impactanalyzer',
+    database: 'mongodb://admin:password@ds117136.mlab.com:17136/authapp',
     secret: 'walangclue'
 }
